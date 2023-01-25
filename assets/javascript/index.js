268c50d9e166aef87006d6e209fc5bd19d1e33e0
Huremovic95 <emirhuric@icloud.com>
2023-01-25 19:15:13 +0000

    Add startgame and cellactivated functions

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,14 +2,36 @@ let gridCells = document.getElementsByClassName("cell");
 let GameOverText = document.getElementById("game-over-text");
 let restart = document.getElementById("restart");
 
-let winCondition = [
+const winCondition = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
     [0, 4, 8], [2, 4, 6] // oblique
 ];
 // creates a array with 9 empty spaces
-let emptySpaces = Array(9).fill(null);
+let spaces = Array(9).fill(null);
 
 let oTurn = "O";
 let xTurn = "X";
 let currentPlayer = xTurn;
+
+function startGame (){
+
+    gridCells.forEach(cell => cell.addEventListener("click", cellActivated));
+
+}
+
+function cellActivated (){
+
+    let cellId = this.getAttribute("data-value");
+    
+    if (spaces[cellId] === null){
+        updateCell(this, cellId)
+    }
+}
+
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+
+startGame();
