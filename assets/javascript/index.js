01b9384a792a887b05c50cfb3851ee4ddc96c539
Huremovic95 <emirhuric@icloud.com>
2023-01-28 20:10:51 +0000

    Add changePlayer function and turn-message div that shows whos turn it is also styled the turn-message div

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,7 @@
 let gridCells = document.getElementsByClassName("cell");
 let GameOverText = document.getElementById("game-over-text");
 let restart = document.getElementById("restart");
+let turnMessage = document.getElementById ("turn-message");
 
 const winCondition = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
@@ -15,21 +16,33 @@ let xTurn = "X";
 let currentPlayer = xTurn;
 
 function startGame (){
-
+    turnMessage.innerText = `${currentPlayer}'s turn`;
+    
     for (let i = 0; i < gridCells.length; i++) {
 		gridCells[i].innerText = '';
-		gridCells[i].addEventListener('click', cellActivated, false);
+		gridCells[i].addEventListener('click', cellActivated);
 }
 }
 
 function cellActivated (cell){
-
+    //cell.target.id takes the specifik id of the cell clicked
     updateCell(cell.target.id, currentPlayer)
 }
 
 function updateCell(cellId, currentPlayer){
     spaces[cellId] = currentPlayer;
     document.getElementById(cellId).innerText = currentPlayer;
+    changePlayer();
+}
+
+function changePlayer(){
+    if (currentPlayer === xTurn){
+        currentPlayer = oTurn;
+    } else {
+    currentPlayer = xTurn
+    }
+
+    turnMessage.innerText = `${currentPlayer}'s turn`;
 }
 
 startGame();
