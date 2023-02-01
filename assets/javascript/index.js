058e5505f3f4d26c86fdc12e7f1cf42ff958d3e2
Huremovic95 <emirhuric@icloud.com>
2023-02-01 20:07:32 +0000

    Add and style a form with 2 player input

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -9,7 +9,7 @@ let spaces = '';
 let winCondition = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
-    [0, 4, 8], [2, 4, 6] // oblique
+    [0, 4, 8], [2, 4, 6] // diagonal
 ];
 
 let gameRunning = false;
